cab92eae182f4590cf2dfd8f8d168f1e01579481
Longinus44 <chijiokelonginus4@gmail.com>
2022-04-20 23:39:51 +0100

    array task

---
+++ array.js
@@ -0,0 +1,10 @@
+
+let first = function(array, n = 1) {
+return  array.slice(0,n);
+
+};
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
